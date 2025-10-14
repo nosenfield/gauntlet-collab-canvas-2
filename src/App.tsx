09e3187207c5f2ba5dd5c1b8848ba26de26bf877
Noah Rosenfield <nrosenfi@gmail.com>
2025-10-14 14:48:07 -0400

    adds project boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <div>
+      <h1>CollabCanvas MVP</h1>
+    </div>
+  );
+}
+
+export default App;
